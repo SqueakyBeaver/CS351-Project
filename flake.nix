af252a7b651d45044e356d5449a0038c8673873a
Beaverr <squeaky.beaver4133@protonmail.com>
2025-04-10 14:54:56 -0500

    Add ui example

--- flake.nix
+++ flake.nix
@@ -3,26 +3,150 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          packages = with pkgs; [ python311 ] ++
-            (with pkgs.python311Packages; [
-              pip
-              venvShellHook
-              dbus-python
-              keyring
-              mysql-connector 
-            ]);
-        };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+
+    /*
+    * Change this value ({major}.{min}) to
+    * update the Python virtual-environment
+    * version. When you do this, make sure
+    * to delete the `.venv` directory to
+    * have the hook rebuild it for the new
+    * version, since it won't overwrite an
+    * existing one. After this, reload the
+    * development shell to rebuild it.
+    * You'll see a warning asking you to
+    * do this when version mismatches are
+    * present. For safety, removal should
+    * be a manual step, even if trivial.
+    */
+    version = "3.12";
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: let
+      concatMajorMinor = v:
+        pkgs.lib.pipe v [
+          pkgs.lib.versions.splitVersion
+          (pkgs.lib.sublist 0 2)
+          pkgs.lib.concatStrings
+        ];
+
+      python = pkgs."python${concatMajorMinor version}";
+      venvDir = ".venv";
+    in {
+      default = pkgs.mkShell {
+        shellHook = ''
+          SOURCE_DATE_EPOCH=$(date +%s)
+
+          if [ -d "${venvDir}" ]; then
+            echo "Skipping venv creation, '${venvDir}' already exists"
+          else
+            echo "Creating new venv environment in path: '${venvDir}'"
+            # Note that the module venv was only introduced in python 3, so for 2.7
+            # this needs to be replaced with a call to virtualenv
+            ${python.interpreter} -m venv "${venvDir}" --copies --system-site-packages
+          fi
+
+          # Under some circumstances it might be necessary to add your virtual
+          # environment to PYTHONPATH, which you can do here too;
+
+          source "${venvDir}/bin/activate"
+
+          runHook postShellHook
+        '';
+
+        postShellHook = ''
+          venvVersionWarn() {
+          	local venvVersion
+          	venvVersion="$("${venvDir}/bin/python" -c 'import platform; print(platform.python_version())')"
+
+          	[[ "$venvVersion" == "${python.version}" ]] && return
+
+          	cat <<EOF
+          Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
+                       Delete '$venvDir' and reload to rebuild for version ${python.version}
+          EOF
+          }
+
+          venvVersionWarn
+
+          # Fucky bash script to patch this son of a bitch
+          # God I hate and love NixOS
+          VENV_PYTHON="$PWD/${venvDir}/bin/python"
+          ORIG_PYTHON="$VENV_PYTHON.orig"
+
+          if [[ -f "$VENV_PYTHON" && ! -f "$ORIG_PYTHON" ]]; then
+            echo "[nix-ld patch] Wrapping .venv/bin/python with LD_LIBRARY_PATH"
+
+            mv "$VENV_PYTHON" "$ORIG_PYTHON"
+
+            cat > "$VENV_PYTHON" <<EOF
+          #!/usr/bin/env bash
+          export LD_LIBRARY_PATH=${pkgs.lib.escapeShellArg "$NIX_LD_LIBRARY_PATH"}
+          exec "$ORIG_PYTHON" "\$@"
+          EOF
+
+            chmod +x "$VENV_PYTHON"
+          else
+            echo "[nix-ld patch] Wrapper already applied or missing .venv/bin/python"
+          fi
+
+        '';
+
+        PYTHONPATH = pkgs.runCommand "pythonpath" {} ''
+          echo $PWD/${venvDir}/${python.sitePackages}/:$PYTHONPATH > $out
+        '';
+
+        NIX_LD_LIBRARY_PATH = with pkgs;
+          lib.makeLibraryPath [
+            glib
+            libGL
+            nss
+            fontconfig
+            nspr
+            dbus
+            zlib
+            zstd
+            stdenv.cc.cc
+            libsodium
+            libxkbcommon
+            freetype
+            xcb-util-cursor
+            xorg.libX11
+            xorg.libxcb
+            xorg.xcbutilwm
+            xorg.xcbutilimage
+            xorg.xcbutilkeysyms
+            xorg.xcbutil
+            xorg.xcbutilrenderutil
+          ];
+
+        # https://github.com/nix-community/nix-ld/issues/19
+        NIX_LD = pkgs.runCommand "ld.so" {} ''
+          ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+        '';
+
+        packages = [
+          (
+            python.withPackages
+            (ps:
+              with ps; [
+                pip
+                keyring
+                mysql-connector
+                pyside6
+                shiboken6
+              ])
+          )
+        ];
+      };
+    });
+  };
 }
---
+++ src/ui/example.py
@@ -0,0 +1,63 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+
+# To run: use the command py -3 src/UI/example.py
+
+
+class ExampleWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hiiiiiiiiiiii :3", "Okay, bye then ;-;"]
+        self.say = ["Go away >:(", "Wait, no come back!"]
+
+        self.button = QtWidgets.QPushButton(self.say[0])
+        self.text = QtWidgets.QLabel(
+            self.hello[0],
+        )
+        self.text.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        
+        self.button.clicked.connect(self.clickButton)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.text)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+
+    @QtCore.Slot()
+    def clickButton(self):
+        if self.text.text() == self.hello[0]:
+            self.text.setText(self.hello[1])
+            self.button.setText(self.say[1])
+        else:
+            self.text.setText(self.hello[0])
+            self.button.setText(self.say[0])
+
+
+# Only run this if this is the main file being run
+# i.e. using `py -3 src.UI/example.py` will execute
+# the code in this if statement
+if __name__ == "__main__":
+    import sys
+    
+    # Create the main application
+    # This should only be done once per application,
+    # so it should be in main.py
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Create a window
+    window = QtWidgets.QMainWindow()
+
+    window.setWindowTitle("Example window")
+
+    # Add our example widget to the window
+    example = ExampleWidget()
+
+    window.setCentralWidget(example)
+
+    # Make the window show up
+    window.show()
+
+    # run the app
+    app.exec()
